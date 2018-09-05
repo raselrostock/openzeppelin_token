131fa788a1a2e7e1fcdb68c19bb3636033a4f663
raselrostock <raselrostock@gmail.com>
2018-09-05 17:48:17 +0300

    Added MyToken file

---
+++ migrations/2_deploy_token.js
@@ -0,0 +1,9 @@
+var MyToken = artifacts.require("./MyToken.sol");
+
+module.exports = function(deployer) {
+	const _name = 'My Token';
+	const _symbol = 'MTK';
+	const _decimals = 18;
+
+    deployer.deploy(MyToken, _name, _symbol, _decimals);
+};
